63e8385b3198861fafeb9140fefc3f3a61dc89a7
ibrohim3 <ibrohim.2128382@gmail.com>
2026-01-03 11:25:39 +0500

    product model added!

---
+++ model/productSchema.js
@@ -0,0 +1,13 @@
+const { required } = require("joi")
+const { Schema, model } = require("mongoose")
+
+const productSchema = new Schema({
+    name: { type: String, required: true, unique: true, trim: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true, trim: true },
+    image: { type: String, required: true },
+    count: { type: Number, default: 0 }
+})
+
+const Product = module("Product", productSchema)
+module.exports = { Product }
