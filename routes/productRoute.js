9b470fee3bb1796927393703933a13471f13be9b
ibrohim3 <ibrohim.2128382@gmail.com>
2026-01-08 20:19:29 +0500

    donee

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,8 +3,70 @@ const product = Router()
 const { productCreate, getProducts, getProduct, updateProduct, deleteProduct, searchProduct } = require("../controller/product.controller")
 const { productCreateValidation, productUpdateValidation } = require("../validation/product.validation")
 const { validate } = require("../middleware/validate")
+
+/**
+ * @swagger
+ *   tags:
+ *    - name: Product
+ *      description: Maxsulotlar
+ */
+/**
+ * @swagger
+ * /products:
+ *   post: 
+ *     tags:
+ *       - Product
+ *     description: Maxsulot qoshish
+ *     requestBody:
+ *       required: true
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: 
+ *               - name
+ *               - price
+ *               - description
+ *               - image
+ *               - count
+ *             properties: 
+ *               name: 
+ *                 type: string
+ *               price:
+ *                 type: number
+ *               description: 
+ *                 type: string
+ *               image: 
+ *                 type: string
+ *               count: 
+ *                 type: number
+ *     responses:
+ *       201:
+ *         description: Maxsulot qo'shildi
+ *       400:
+ *         description: Xato so'rov yoki validate xatosi
+ *       500:
+ *         description: Server xatosi
+ */
 product.post("/", validate(productCreateValidation, 'body'), productCreate)
+
+/**
+ * @swagger
+ * /products:
+ *   get:   
+ *     summary: Barcha maxsulotlar ro'yxatini olish
+ *     tags: 
+ *       - Product
+ *     responses:
+ *       200: 
+ *         description: Barcha maxsulotlar royxati olindi
+ *       404:
+ *         description: Maxsulot topilmadi
+ *       500: 
+ *         description: Server xatosi
+ */
 product.get("/", getProducts)
+
 product.get("/search", searchProduct)
 product.get("/:id", getProduct)
 product.patch("/:id", validate(productUpdateValidation, 'body'), updateProduct)
